08c88525980d11ac0485c836e327ab4c9f95784a
mek <151859462+darthsitharian@users.noreply.github.com>
2026-09-18 11:33:21 +0200

    Use theme grid for featured posts

--- patterns/featured-posts.php
+++ patterns/featured-posts.php
@@ -9,7 +9,7 @@
 <!-- wp:group {"className":"cm-featured-posts","layout":{"type":"default"}} -->
 <div class="wp-block-group cm-featured-posts">
 
-	<!-- wp:query {"queryId":1,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"displayLayout":{"type":"grid","columns":12},"className":"cm-featured-query"} -->
+	<!-- wp:query {"queryId":1,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"className":"cm-featured-query"} -->
 	<div class="wp-block-query cm-featured-query">
 
 		<!-- wp:post-template {"className":"cm-grid cm-featured-posts-grid","layout":{"type":"default"}} -->
