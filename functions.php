dca97abe369f8492cd485d22541e59c9b902a8c8
mek <151859462+darthsitharian@users.noreply.github.com>
2026-09-18 12:13:14 +0200

    Filter Zyskomat Query Loop by Polecamy category

--- functions.php
+++ functions.php
@@ -29,3 +29,26 @@ function calymyk_new_enqueue_assets() {
 }
 
 add_action( 'wp_enqueue_scripts', 'calymyk_new_enqueue_assets' );
+
+/**
+ * Limit the Zyskomat recommendation Query Loop to posts
+ * assigned to the standard "Polecamy" category.
+ *
+ * The category stays in WordPress core, so editors can simply
+ * tick "Polecamy" while creating or editing a post.
+ */
+function calymyk_new_recommendations_query( $query, $block, $page ) {
+
+	if (
+		! isset( $block->parsed_block['attrs']['namespace'] )
+		|| 'calymyk-new/recommendations' !== $block->parsed_block['attrs']['namespace']
+	) {
+		return $query;
+	}
+
+	$query['category_name'] = 'polecamy';
+
+	return $query;
+}
+
+add_filter( 'query_loop_block_query_vars', 'calymyk_new_recommendations_query', 10, 3 );
