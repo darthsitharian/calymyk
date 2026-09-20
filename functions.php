f139f9ad3b3959ac8368953c3bd00ed6d853a135
mek <151859462+darthsitharian@users.noreply.github.com>
2026-09-20 12:31:49 +0200

    Render promotion step chevron as inline SVG

--- functions.php
+++ functions.php
@@ -285,7 +285,7 @@ function calymyk_new_render_promotion_steps( $attributes ) {
 		$fields      = isset( $step['fields'] ) && is_array( $step['fields'] ) ? $step['fields'] : array();
 
 		$output .= '<details class="cm-promotion-step">';
-		$output .= '<summary class="cm-promotion-step__toggle"><span class="cm-promotion-step__header"><span class="cm-promotion-step__number">' . esc_html( str_pad( (string) ( $index + 1 ), 2, '0', STR_PAD_LEFT ) ) . '</span><span class="cm-promotion-step__heading"><span class="cm-promotion-step__eyebrow">KROK ' . esc_html( $index + 1 ) . '</span><span class="cm-promotion-step__title">' . esc_html( $title ) . '</span></span></span><span class="cm-promotion-step__toggle-icon cm-icon cm-icon--chevron" aria-hidden="true"></span></summary>';
+		$output .= '<summary class="cm-promotion-step__toggle"><span class="cm-promotion-step__header"><span class="cm-promotion-step__number">' . esc_html( str_pad( (string) ( $index + 1 ), 2, '0', STR_PAD_LEFT ) ) . '</span><span class="cm-promotion-step__heading"><span class="cm-promotion-step__eyebrow">KROK ' . esc_html( $index + 1 ) . '</span><span class="cm-promotion-step__title">' . esc_html( $title ) . '</span></span></span><svg class="cm-promotion-step__toggle-icon" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="m6 9 6 6 6-6"></path></svg></summary>';
 		$output .= '<div class="cm-promotion-step__body">';
 
 		if ( $description ) {
