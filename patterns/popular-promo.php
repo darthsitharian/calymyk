43ce83e9f9d2ed8a3fd04036d058ae18b627cabb
mek <151859462+darthsitharian@users.noreply.github.com>
2026-09-23 11:02:39 +0200

    Dodaj pustą sekcję popular-promo

---
+++ patterns/popular-promo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Popular Promo
+ * Slug: calymyk-new/popular-promo
+ * Inserter: false
+ */
+?>
+
+<!-- wp:group {"className":"cm-popular-promo","layout":{"type":"default"}} -->
+<div class="wp-block-group cm-popular-promo">
+</div>
+<!-- /wp:group -->
