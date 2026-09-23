9efc54dd18744dc8b973c94b4f468507709c81ff
mek <151859462+darthsitharian@users.noreply.github.com>
2026-09-23 11:02:41 +0200

    Dodaj pustą sekcję warto-wiedziec

---
+++ patterns/warto-wiedziec.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Warto Wiedzieć
+ * Slug: calymyk-new/warto-wiedziec
+ * Inserter: false
+ */
+?>
+
+<!-- wp:group {"className":"cm-warto-wiedziec","layout":{"type":"default"}} -->
+<div class="wp-block-group cm-warto-wiedziec">
+</div>
+<!-- /wp:group -->
