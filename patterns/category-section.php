cdf8e6b03d55973071f831386e3ed486c1631a31
mek <151859462+darthsitharian@users.noreply.github.com>
2026-09-23 15:57:27 +0200

    Link homepage categories to category pages

--- patterns/category-section.php
+++ patterns/category-section.php
@@ -9,12 +9,12 @@
 <!-- wp:group {"className":"cm-category-section","layout":{"type":"default"}} -->
 <div class="wp-block-group cm-category-section">
 	<div class="cm-category-section__grid">
-		<a class="cm-category-section__item" href="#">AI</a>
-		<a class="cm-category-section__item" href="#">Handel</a>
-		<a class="cm-category-section__item" href="#">Biznes</a>
-		<a class="cm-category-section__item" href="#">Marketing</a>
-		<a class="cm-category-section__item" href="#">Tech</a>
-		<a class="cm-category-section__item" href="#">Finanse</a>
+		<a class="cm-category-section__item" href="/category/ai/">AI</a>
+		<a class="cm-category-section__item" href="/category/handel/">Handel</a>
+		<a class="cm-category-section__item" href="/category/biznes/">Biznes</a>
+		<a class="cm-category-section__item" href="/category/marketing/">Marketing</a>
+		<a class="cm-category-section__item" href="/category/tech/">Tech</a>
+		<a class="cm-category-section__item" href="/category/finanse/">Finanse</a>
 	</div>
 </div>
 <!-- /wp:group -->
