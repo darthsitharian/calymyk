05b416bb75e7a70edb4da7bc40e0d9bfac62fa95
mek <151859462+darthsitharian@users.noreply.github.com>
2026-09-23 11:02:34 +0200

    Dodaj pustą sekcję category-section

---
+++ patterns/category-section.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Category Section
+ * Slug: calymyk-new/category-section
+ * Inserter: false
+ */
+?>
+
+<!-- wp:group {"className":"cm-category-section","layout":{"type":"default"}} -->
+<div class="wp-block-group cm-category-section">
+</div>
+<!-- /wp:group -->
