4c44f8ee5ace63c8ab717dd46a469e41fecf9b99
mek <151859462+darthsitharian@users.noreply.github.com>
2026-09-23 11:22:08 +0200

    Add category grid boxes

--- patterns/category-section.php
+++ patterns/category-section.php
@@ -8,5 +8,13 @@
 
 <!-- wp:group {"className":"cm-category-section","layout":{"type":"default"}} -->
 <div class="wp-block-group cm-category-section">
+	<div class="cm-category-section__grid">
+		<a class="cm-category-section__item" href="#">AI</a>
+		<a class="cm-category-section__item" href="#">Handel</a>
+		<a class="cm-category-section__item" href="#">Biznes</a>
+		<a class="cm-category-section__item" href="#">Marketing</a>
+		<a class="cm-category-section__item" href="#">Tech</a>
+		<a class="cm-category-section__item" href="#">Finanse</a>
+	</div>
 </div>
 <!-- /wp:group -->
